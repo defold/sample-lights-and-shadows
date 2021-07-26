6ba5076405b4b804251792cb8c56afbc94b8a94e
Björn Ritzl <bjorn.ritzl@gmail.com>
2021-07-26 08:37:53 +0200

    Updated example and cleaned up code

---
+++ example/ball.go
@@ -0,0 +1,100 @@
+components {
+  id: "ball"
+  component: "/example/ball.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "lightsource"
+  component: "/lights/lightsource.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  properties {
+    id: "color"
+    value: "1.0, 0.0, 0.0, 0.5"
+    type: PROPERTY_TYPE_VECTOR4
+  }
+}
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_DYNAMIC\n"
+  "mass: 50.0\n"
+  "friction: 0.4\n"
+  "restitution: 0.5\n"
+  "group: \"ball\"\n"
+  "mask: \"block\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_SPHERE\n"
+  "    position {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 1\n"
+  "  }\n"
+  "  data: 16.0\n"
+  "}\n"
+  "linear_damping: 0.3\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/example/asset/img.atlas\"\n"
+  "default_animation: \"ball_red_small_alt\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
--- lights/lights.lua
+++ lights/lights.lua
@@ -9,24 +9,10 @@ local shadow_map_target = nil
 local quad_pred = nil
 local clear_color = nil
 
-function M.init()
-	local color_params = { format = render.FORMAT_RGBA,
-							width = 64,
-							height = 64,
-							min_filter = render.FILTER_LINEAR,
-							mag_filter = render.FILTER_LINEAR,
-							u_wrap = render.WRAP_CLAMP_TO_EDGE,
-							v_wrap = render.WRAP_CLAMP_TO_EDGE }
 
-	occluder_target = render.render_target({[render.BUFFER_COLOR_BIT] = color_params})
-	shadow_map_target = render.render_target({[render.BUFFER_COLOR_BIT] = color_params})
-	quad_pred = render.predicate({ "light_quad" })
-	clear_color = vmath.vector4(0,0,0,1)
-end
+local DEFAULT_LIGHT_SIZE = 256
+local WHITE = vmath.vector4(1, 1, 1, 1)
 
-function M.set_clear_color(color)
-	clear_color = color
-end
 
 -- draw quad using the light_map material
 -- with shadow_map as input texture
@@ -42,14 +28,13 @@ local function draw_light(light, view, projection, window_width, window_height, 
 
 	local constants = render.constant_buffer()
 	constants.light_pos = vmath.vector4(light.position.x, light.position.y, light.position.z, 0)
-	constants.size = vmath.vector4(light.size.x, light.size.y, light.size.z, 0)
+	constants.size = vmath.vector4(light.size, light.size, 1, 0)
 	-- Color clamped to 0.0, 1.0
 	constants.color = light.color
 	constants.property = vmath.vector4(light.radial_falloff, 0, 0, 0)
 	constants.angle = vmath.vector4(light.angle.x, light.angle.y, light.angle.z, light.angle.w)
 
-	-- Call blend_func before draw
-	blend_func()
+	if blend_func then blend_func() end
 	render.draw(quad_pred, constants)
 
 	render.disable_texture(0, shadow_map_target)
@@ -59,20 +44,20 @@ end
 
 local function draw_occluder(light, view, projection, occluder_predicate)
 	-- Set render target size to precision
-	render.set_render_target_size(occluder_target, light.size_scaled.x, light.size_scaled.y)
+	render.set_render_target_size(occluder_target, light.size_scaled, light.size_scaled)
 
 	-- Set viewport
-	render.set_viewport(0, 0, light.size_scaled.x, light.size_scaled.y)
+	render.set_viewport(0, 0, light.size_scaled, light.size_scaled)
 
 	-- Set projection so occluders fill the render target
-	render.set_projection(vmath.matrix4_orthographic(0, light.size.x, 0, light.size.y, -5, 5))
+	render.set_projection(vmath.matrix4_orthographic(0, light.size, 0, light.size, -5, 5))
 
 	-- Set view matrix to light position
 	render.set_view(
-		vmath.matrix4_look_at(
-		vmath.vector3(-light.size_half.x, -light.size_half.y, 0) + light.position, 
-		vmath.vector3(-light.size_half.x, -light.size_half.y, -1) + light.position,
-		vmath.vector3(0, 1, 0)))
+	vmath.matrix4_look_at(
+	vmath.vector3(-light.size_half, -light.size_half, 0) + light.position, 
+	vmath.vector3(-light.size_half, -light.size_half, -1) + light.position,
+	vmath.vector3(0, 1, 0)))
 
 	-- Clear then draw
 	render.set_render_target(occluder_target, { transient = { render.BUFFER_DEPTH_BIT, render.BUFFER_STENCIL_BIT } } )
@@ -91,20 +76,20 @@ end
 
 local function draw_shadow_map(light)
 	-- Set render target size to precision
-	render.set_render_target_size(shadow_map_target, light.size_scaled.x, 1)
+	render.set_render_target_size(shadow_map_target, light.size_scaled, 1)
 
 	-- Set viewport
-	render.set_viewport(0, 0, light.size_scaled.x, light.size_scaled.y)
-	
+	render.set_viewport(0, 0, light.size_scaled, light.size_scaled)
+
 	-- Set projection so occluders fill the render target
-	render.set_projection(vmath.matrix4_orthographic(0, light.size.x, 0, 1, -5, 5))
+	render.set_projection(vmath.matrix4_orthographic(0, light.size, 0, 1, -5, 5))
 
 	-- Set view matrix to middle
 	render.set_view(
-		vmath.matrix4_look_at(
-		vmath.vector3(-light.size_half.x, -light.size_half.y, 0),
-		vmath.vector3(-light.size_half.x, -light.size_half.y, -1),
-		vmath.vector3(0, 1, 0)))
+	vmath.matrix4_look_at(
+	vmath.vector3(-light.size_half, -light.size_half, 0),
+	vmath.vector3(-light.size_half, -light.size_half, -1),
+	vmath.vector3(0, 1, 0)))
 
 	-- Clear then draw
 	render.set_render_target(shadow_map_target, { transient = { render.BUFFER_DEPTH_BIT, render.BUFFER_STENCIL_BIT } } )
@@ -115,14 +100,36 @@ local function draw_shadow_map(light)
 
 	-- Only resolution.x
 	local constants = render.constant_buffer()
-	constants.resolution = vmath.vector4(light.size_scaled.x)
-	constants.size = vmath.vector4(light.size.x, light.size.y, light.size.z, 0)
+	constants.resolution = vmath.vector4(light.size_scaled)
+	constants.size = vmath.vector4(light.size, light.size, 1, 0)
 	render.draw(quad_pred, constants)
 
 	render.disable_texture(0, occluder_target)
 	render.disable_material()
 end
 
+
+function M.init(config)
+	local target_size = config and config.render_target_size or 64
+
+	local color_params = {
+		format = render.FORMAT_RGBA,
+		width = target_size,
+		height = target_size,
+		min_filter = render.FILTER_LINEAR,
+		mag_filter = render.FILTER_LINEAR,
+		u_wrap = render.WRAP_CLAMP_TO_EDGE,
+		v_wrap = render.WRAP_CLAMP_TO_EDGE
+	}
+	occluder_target = render.render_target({[render.BUFFER_COLOR_BIT] = color_params})
+	shadow_map_target = render.render_target({[render.BUFFER_COLOR_BIT] = color_params})
+	quad_pred = render.predicate({ "light_quad" })
+	clear_color = vmath.vector4(0,0,0,1)
+end
+
+function M.set_clear_color(color)
+	clear_color = color
+end
 
 function M.draw(view, projection, occluder_predicate)
 	local window_width = render.get_window_width()
@@ -137,22 +144,27 @@ function M.draw(view, projection, occluder_predicate)
 	end
 end
 
+
 function M.add(properties)
 	assert(properties)
+	assert(properties.radius, "You must specify a radius")
+	assert(properties.position, "You must specify a position")
+
 	id = id + 1
 
-	lights[id] = {
-		position = properties.position,
-		size = properties.size,
-		precision = properties.precision,
-		color = properties.color,
-		angle = properties.angle,
-		radial_falloff = properties.radial_falloff,
+	local size = properties.radius * 2
+	local precision = DEFAULT_LIGHT_SIZE / size
 
-		-- Defined in runtime
+	lights[id] = {
 		id = id,
-		size_half = properties.size * 0.5,
-		size_scaled = properties.size * properties.precision,
+		position = properties.position,
+		size = size,
+		precision = precision,
+		color = properties.color or WHITE,
+		angle = properties.angle or 360,
+		radial_falloff = properties.radial_falloff or 1,
+		size_half = size * 0.5,
+		size_scaled = size * precision,
 	}
 
 	return id
@@ -164,14 +176,16 @@ function M.remove(id)
 	lights[id] = nil
 end
 
-function M.set_light_size(id, size)
+function M.set_light_radius(id, radius)
 	assert(id)
 	assert(lights[id], "Unable to find light")
-	assert(size)
+	assert(radius)
 	local light = lights[id]
+	local size = radius * 2
+	local precision = DEFAULT_LIGHT_SIZE / size
 	light.size = size
 	light.size_half = size * 0.5
-	light.size_scaled = size * light.precision
+	light.size_scaled = size * precision
 end
 
 function M.set_position(id, position)
